25323f93739d658e28a337fb9f4497a7e8a019d1
Sean Reilly <seanreilly444@gmail.com>
2026-02-18 10:57:21 +0000

    Initial Docusaurus setup

--- my-docusaurus-site/docusaurus.config.js
+++ my-docusaurus-site/docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'MyLearning Diary',
+  tagline: 'Documenting my programming journey',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
@@ -20,15 +20,15 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://seanreilly34.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/plc-docusaurus-site/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'seanreilly34', // Usually your GitHub org/user name.
+  projectName: 'plc-docusaurus-site', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
